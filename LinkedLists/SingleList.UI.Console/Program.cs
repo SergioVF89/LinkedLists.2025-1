b8717adf5e19d439e9dd6db6d9f9a8665a4095ff
Zulu <jzuluaga55@gmail.com>
2025-04-10 20:27:40 -0500

    Single lists

---
+++ LinkedLists/SingleList.UI.Console/Program.cs
@@ -0,0 +1,77 @@
+﻿using SingleList;
+
+var singleList = new SinglyLinkedList<string>();
+var opc = "0";
+do
+{
+    opc = Menu();
+    switch (opc)
+    {
+        case "1":
+            Console.Write("Enter the data to insert at the beginning: ");
+            var dataAtBeginning = Console.ReadLine();
+            if (dataAtBeginning != null)
+            {
+                singleList.InsertAtBeginning(dataAtBeginning);
+            }
+            break;
+
+        case "2":
+            Console.Write("Enter the data to insert at the end: ");
+            var dataAtEnd = Console.ReadLine();
+            if (dataAtEnd != null)
+            {
+                singleList.InsertAtEnd(dataAtEnd);
+            }
+            break;
+
+        case "3":
+            Console.WriteLine(singleList.ToString());
+            break;
+
+        case "4":
+            Console.Write("Enter the data to validate if exists: ");
+            var data = Console.ReadLine();
+            if (data != null)
+            {
+                var anwser = singleList.Contains(data) ? "exists" : "doesn't exists";
+                Console.WriteLine($"The data: {data} {anwser}");
+            }
+            break;
+
+        case "5":
+            singleList.Clear();
+            Console.Write("List cleaned.");
+            break;
+
+        case "6":
+            Console.Write("Enter the data to remove: ");
+            var remove = Console.ReadLine();
+            if (remove != null)
+            {
+                if (singleList.Contains(remove))
+                {
+                    singleList.Remove(remove);
+                    Console.WriteLine("Item removed.");
+                }
+                else
+                {
+                    Console.WriteLine("Item doesn't exists.");
+                }
+            }
+            break;
+    }
+} while (opc != "0");
+
+string Menu()
+{
+    Console.WriteLine("1. Insert at beginning");
+    Console.WriteLine("2. Insert at end");
+    Console.WriteLine("3. Show list");
+    Console.WriteLine("4. Contains");
+    Console.WriteLine("5. Clear all list");
+    Console.WriteLine("6. Remove item");
+    Console.WriteLine("0. Exit");
+    Console.Write("Choose an option: ");
+    return Console.ReadLine() ?? "0";
+}
---
+++ LinkedLists/SingleList/SinglyLinkedList.cs
@@ -0,0 +1,91 @@
+﻿namespace SingleList;
+
+public class SinglyLinkedList<T>
+{
+    private SingleNode<T>? head;
+
+    public SinglyLinkedList()
+    {
+        head = null;
+    }
+
+    public override string ToString()
+    {
+        var output = string.Empty;
+        var currentNode = head;
+        while (currentNode != null)
+        {
+            output += $"{currentNode.Data} -> ";
+            currentNode = currentNode.Next;
+        }
+        output += "null";
+        return output;
+    }
+
+    public void InsertAtBeginning(T data)
+    {
+        var newNode = new SingleNode<T>(data);
+        newNode.Next = head;
+        head = newNode;
+    }
+
+    public void InsertAtEnd(T data)
+    {
+        var newNode = new SingleNode<T>(data);
+        if (head == null)
+        {
+            head = newNode;
+            return;
+        }
+
+        var current = head;
+        while (current.Next != null)
+        {
+            current = current.Next;
+        }
+        current.Next = newNode;
+    }
+
+    public bool Contains(T data)
+    {
+        var current = head;
+        while (current != null)
+        {
+            if (current.Data!.Equals(data))
+            {
+                return true;
+            }
+            current = current.Next;
+        }
+        return false;
+    }
+
+    public void Clear()
+    {
+        head = null;
+    }
+
+    public void Remove(T data)
+    {
+        if (head == null)
+        {
+            return;
+        }
+
+        if (head.Data!.Equals(data))
+        {
+            head = head.Next;
+            return;
+        }
+
+        var current = head;
+        while (current.Next != null && !current.Next.Data!.Equals(data))
+        {
+            current = current.Next;
+        }
+        if (current.Next != null)
+        {
+            current.Next = current.Next.Next;
+        }
+    }
+}
